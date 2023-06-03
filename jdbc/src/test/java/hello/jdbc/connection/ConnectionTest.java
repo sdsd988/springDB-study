92d14ff93857e6d67e00200c100fa08a228fec19
JUNGSANGYUN <sdsd98987@gmail.com>
2023-06-03 16:39:55 +0900

    [SpringDB] 1장 JDBC 이해

---
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,13 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager(){
+
+    }
+}
