a4e677ab5919f630910531c9158beec810cc26d1
JUNGSANGYUN <sdsd98987@gmail.com>
2023-06-10 12:29:01 +0900

    [SpringDB2] MyBatis, JPA

--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,16 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
